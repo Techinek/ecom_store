39e723695dfc6c4e08492bd46b3927507e8325a9
Andrey Kuskov <techinek@gmail.com>
2022-03-28 10:40:51 +0300

    Add settings for django-debug. Add dummy seed.sql to populate data

---
+++ README.md
@@ -0,0 +1,10 @@
+# StoreFront
+
+Ecommerce project for placing orders with unique features:
+- product tagging
+- product collections
+- product promotions
+- wish lists
+
+## Dummy data is included:
+- just run seed.sql to populate model instances.
--- storefront/settings.py
+++ storefront/settings.py
@@ -143,6 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # IP for Django-debug toolbar
 
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
+if DEBUG:
+    import socket
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
