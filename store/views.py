51c9217bd93795c1f3895ca46267081233952309
Andrey Kuskov <techinek@gmail.com>
2022-04-06 10:10:22 +0300

    add restrictions for methods in OrderViewSet

--- store/views.py
+++ store/views.py
@@ -6,7 +6,7 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.mixins import (CreateModelMixin, DestroyModelMixin,
                                    RetrieveModelMixin)
 from rest_framework.permissions import (IsAuthenticated,
-                                        DjangoModelPermissions)
+                                        DjangoModelPermissions, IsAdminUser)
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
@@ -110,7 +110,12 @@ class CustomerViewSet(ModelViewSet):
 
 
 class OrderViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    http_method_names = ['get', 'patch', 'delete', 'head', 'options']
+
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
 
     def create(self, request, *args, **kwargs):
         serializer = (CreateOrderSerializer(data=request.data,
