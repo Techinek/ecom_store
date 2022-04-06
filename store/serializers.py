d766b1515b0195d9605e842a5c7de324b5c3ebb4
Andrey Kuskov <techinek@gmail.com>
2022-04-06 09:54:51 +0300

    Refactor OrderViewSet: add create method

--- store/serializers.py
+++ store/serializers.py
@@ -146,3 +146,4 @@ class CreateOrderSerializer(serializers.Serializer):
                            for item in cart_items]
             OrderItem.objects.bulk_create(order_items)
             Cart.objects.filter(pk=self.validated_data['cart_id']).delete()
+            return order
