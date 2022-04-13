ad56cb77be513b12ea8c6cce4e5159745231fbb8
Andrey Kuskov <techinek@gmail.com>
2022-04-13 16:35:49 +0300

    Added TestRetrieveCollection

--- store/tests/conftest.py
+++ store/tests/conftest.py
@@ -1,7 +1,17 @@
+from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
 import pytest
+
+User = get_user_model()
 
 
 @pytest.fixture
 def api_client():
     return APIClient()
+
+
+@pytest.fixture
+def authenticate(api_client):
+    def authenticate_role_user(is_staff=False):
+        return api_client.force_authenticate(user=User(is_staff=is_staff))
+    return authenticate_role_user
--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,9 +1,9 @@
+from model_bakery import baker
 import pytest
-from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.reverse import reverse
 
-User = get_user_model()
+from store.models import Collection
 
 
 @pytest.fixture
@@ -15,23 +15,40 @@ def create_collection(api_client):
 
 @pytest.mark.django_db
 class TestCreateCollection:
-    def test_anonymous_user_gets_401(self, api_client, create_collection):
+    def test_anonymous_user_gets_401(self, create_collection):
         response = create_collection({'title': 'a'})
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_not_admin_user_gets_403(self, api_client, create_collection):
-        api_client.force_authenticate(user={})
+    def test_not_admin_user_gets_403(self, api_client,
+                                     create_collection, authenticate):
+        authenticate()
         response = create_collection({'title': 'a'})
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_invalid_data_returns_400(self, api_client, create_collection):
-        api_client.force_authenticate(user=User(is_staff=True))
+    def test_invalid_data_returns_400(self, api_client,
+                                      create_collection, authenticate):
+        authenticate(is_staff=True)
         response = create_collection({'title': ''})
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['title'] is not None
 
-    def test_valid_data_returns_200(self, api_client, create_collection):
-        api_client.force_authenticate(user=User(is_staff=True))
+    def test_valid_data_returns_200(self, api_client,
+                                    create_collection, authenticate):
+        authenticate(is_staff=True)
         response = create_collection({'title': 'a'})
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
+
+
+@pytest.mark.django_db
+class TestRetrieveCollection:
+    def test_existed_collection_returns_200(self, api_client):
+        collection = baker.make(Collection)
+        response = api_client.get(reverse('store:collections-detail',
+                                          args=[collection.id]))
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': collection.id,
+            'title': collection.title,
+            'products_count': 0
+        }
